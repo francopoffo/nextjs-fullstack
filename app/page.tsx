2a4e2f0691fde88bcd12b0f5831d626d68b540b2
Franco Poffo <franco_sutter@yahoo.com.br>
2023-05-23 18:36:10 -0300

    Criando a rota de API para renderização dos posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,29 @@
 "use client";
 
+import axios from "axios";
 import AddPostForm from "./components/AddPostForm";
+import { useQuery } from "@tanstack/react-query";
 
 type Props = {};
 
+const allPosts = async () => {
+  const response = await axios.get("/api/posts/getPosts");
+  return response.data;
+};
+
 const Home = () => {
+  const { data, error, isLoading } = useQuery({
+    queryFn: allPosts,
+    queryKey: ["posts"],
+  });
+
+  if (error) return error;
+  if (isLoading) return <p>Loading...</p>;
+
   return (
-    <section>
+    <main>
       <AddPostForm />
-    </section>
+    </main>
   );
 };
 
